f85ad945c964bf18802638609948560fbda25995
N <nirav2000+flatiron@gmail.com>
2018-05-15 21:46:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function shout(string) {
+<<<<<<< HEAD
   return string.toUpperCase();
 }
 
@@ -14,10 +15,28 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase());
   return;
+=======
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+  return
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+  return
+>>>>>>> 00502985cbc14bec82dbd067e0127ab14913119d
 }
 
 function sayHiToGrandma(string) {
     if (string.toLowerCase() === string) {
+<<<<<<< HEAD
       return "I can\'t hear you!";
     }
    
@@ -30,3 +49,17 @@ function sayHiToGrandma(string) {
   }
   return;
 }
+=======
+      return "I can\'t hear you!"
+    }
+   
+    if (string.toUpperCase() === string) {
+      return "YES INDEED!"
+    }
+
+  if ("I love you, Grandma." === string) {
+    return "I love you, too."
+  }
+  return
+}
+>>>>>>> 00502985cbc14bec82dbd067e0127ab14913119d
